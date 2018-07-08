c446734d8c3f0d3b4c3fd36cb74703a6186e0af5
Fernando Rosendo <fernandolgl75@gmail.com>
2018-07-07 21:24:38 -0300

    esqueleto

---
+++ ejepe-data.sql
@@ -0,0 +1,17 @@
+INSERT INTO participante
+VALUES ();
+
+INSERT INTO trabalhador
+VALUES ();
+
+INSERT INTO quarto
+VALUES ();
+
+INSERT INTO oficina 
+VALUES ();
+
+INSERT INTO alimentacao
+VALUES ();
+
+INSERT INTO equipe
+VALUES ();
---
+++ ejepe-ddl.sql
@@ -0,0 +1,23 @@
+CREATE TABLE participante(
+
+);
+
+CREATE TABLE trabalhador(
+
+);
+
+CREATE TABLE quarto(
+
+);
+
+CREATE TABLE oficina(
+
+);
+
+CREATE TABLE alimentacao(
+
+);
+
+CREATE TABLE equipe(
+
+);
