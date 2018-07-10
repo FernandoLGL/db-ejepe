712957596984293e2457b641653c3e3d355c2797
Fernando Rosendo <fernandolgl75@gmail.com>
2018-07-10 08:25:47 -0300

    ddl com chaves

--- ejepe-ddl.sql
+++ ejepe-ddl.sql
@@ -1,23 +1,29 @@
 CREATE TABLE participante(
-
-);
-
-CREATE TABLE trabalhador(
-
-);
-
-CREATE TABLE quarto(
-
+id_participante serial PRIMARY KEY
 );
 
 CREATE TABLE oficina(
-
-);
-
-CREATE TABLE alimentacao(
-
+id_oficina serial PRIMARY KEY,
+id_participante serial REFERENCES participante(id_participante)
 );
 
 CREATE TABLE equipe(
+id_equipe serial PRIMARY KEY
+);
 
+CREATE TABLE trabalhador(
+id_trabalhador serial PRIMARY KEY,
+id_equipe serial REFERENCES equipe(id_equipe)
+);
+
+CREATE TABLE quarto(
+id_quarto serial PRIMARY KEY,
+id_trabalhador serial REFERENCES trabalhador(id_trabalhador),
+id_participante serial REFERENCES participante(id_participante)
+);
+
+CREATE TABLE alimentacao(
+id_alimentacao serial PRIMARY KEY,
+id_participante serial REFERENCES participante(id_participante),
+id_trabalhador serial REFERENCES trabalhador(id_trabalhador)
 );
